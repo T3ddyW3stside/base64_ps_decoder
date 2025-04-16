3c1c0c5b29b55237e54adad196888ecb0cddc957
T3ddyW3stside <145967677+T3ddyW3stside@users.noreply.github.com>
2025-04-16 05:24:13 +0200

    Update create_file.ps1

--- create_file.ps1
+++ create_file.ps1
@@ -1,14 +1,13 @@
-# Create Base64 file
 
-$filePath = "C:\Users\mbr\Downloads\tor-browser-windows-x86_64-portable-13.5.2.exe"  # Ändere dies zum richtigen Pfad der .exe-Datei
-$outputFile = "C:\Users\mbr\Desktop\datei_base64.txt"  # Pfad zur Base64-Textdatei
+$filePath = "C:\Users\mbr\Downloads\tor-browser-windows-x86_64-portable-13.5.2.exe"  #Change Path
+$outputFile = "C:\Users\mbr\Desktop\datei_base64.txt"  #Change Path
 
-# Prüfe, ob die Datei existiert
+#Check if file exist
 if (Test-Path $filePath) {
-    # Konvertiere die Datei in einen Base64-String
+    # Convert file
     $base64 = [Convert]::ToBase64String([IO.File]::ReadAllBytes($filePath))
 
-    # Speichere den Base64-String in eine Textdatei
+    # Save File
     Set-Content -Path $outputFile -Value $base64
     Write-Host "Base64-Umwandlung erfolgreich! Datei gespeichert unter: $outputFile"
 } else {
